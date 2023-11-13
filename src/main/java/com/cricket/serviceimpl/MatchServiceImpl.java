b21b7f469dcff7b2efc1721005dc85627ba0d965
Harish Kumar R <53997952+harishhkjg@users.noreply.github.com>
2023-11-13 20:24:00 +0530

    Batting TeamName Added

--- src/main/java/com/cricket/serviceimpl/MatchServiceImpl.java
+++ src/main/java/com/cricket/serviceimpl/MatchServiceImpl.java
@@ -70,23 +70,138 @@ public class MatchServiceImpl implements MatchService {
         int InningsScore = 0;
         int wicketCount = 0;
         List<String> EachBall = new ArrayList<>();
+        List<String> batsmanNamedata = new ArrayList<>();
+
         for (int ball = 1; ball <= totalBalls && wicketCount<Playersize; ball++) {
             int StoreBalls = RandomBallGenerator();
             if (StoreBalls == 7) {
                 wicketCount++;
+                batsmanNamedata.add(BatsmanName(wicketCount,BattingTeam));
                 EachBall.add("W");
-
             } else {
                 InningsScore += StoreBalls;
                 EachBall.add(String.valueOf(StoreBalls));
             }
         }
 
-        match.addTeamResult(BattingTeam.getTeamname(), InningsScore, wicketCount, EachBall);
+
+        match.addTeamResult(BattingTeam.getTeamname(), InningsScore, wicketCount, EachBall, batsmanNamedata);
     }
     private int RandomBallGenerator() {
 
         return (int) (Math.random() * 8);
+    }
+
+    private String BatsmanName(int wicketCount, Team battingTeam)
+    {
+
+        String teamname = battingTeam.getTeamname();
+        System.out.println(teamname);
+        List<String> batsmanName = new ArrayList<>();
+
+        if(teamname.equalsIgnoreCase("india")) {
+            switch (wicketCount) {
+                case 1:
+                    batsmanName.add("Shubman Gill");
+
+                    break;
+                case 2:
+                    batsmanName.add("Rohit Sharma");
+
+                    break;
+                case 3:
+                    batsmanName.add("Virat Kohli");
+
+                    break;
+                case 4:
+                    batsmanName.add("shreyas Iyer");
+
+                    break;
+                case 5:
+                    batsmanName.add("KL Rahul");
+
+                    break;
+                case 6:
+                    batsmanName.add("Suryakumar Yadav");
+
+                    break;
+                case 7:
+                    batsmanName.add("Jadeja");
+
+                    break;
+                case 8:
+                    batsmanName.add("Kuldeep Yadav");
+
+                    break;
+                case 9:
+                    batsmanName.add("Mohammad Shami");
+
+                    break;
+                case 10:
+                    batsmanName.add("Jasprit Bumrah");
+
+                    break;
+                case 11:
+                    batsmanName.add("Mohammad Siraj");
+
+                    break;
+            }
+
+            return batsmanName.toString();
+        }
+        else if(teamname.equalsIgnoreCase("nz")) {
+            switch (wicketCount) {
+
+                case 1:
+                    batsmanName.add("Devon Conway");
+
+                    break;
+                case 2:
+                    batsmanName.add("Rachin Ravindra");
+
+                    break;
+                case 3:
+                    batsmanName.add("young");
+
+                    break;
+                case 4:
+                    batsmanName.add("Kane Wiliamson");
+
+                    break;
+                case 5:
+                    batsmanName.add("Dary Mitchell");
+
+                    break;
+                case 6:
+                    batsmanName.add("Glenn Philips");
+
+                    break;
+                case 7:
+                    batsmanName.add("Tom Latham");
+
+                    break;
+                case 8:
+                    batsmanName.add("Mitchell Santner");
+
+                    break;
+                case 9:
+                    batsmanName.add("Tim Southee");
+
+                    break;
+                case 10:
+                    batsmanName.add("Trent Boult");
+
+                    break;
+                case 11:
+                    batsmanName.add("Matt Henry");
+
+                    break;
+            }
+
+            return batsmanName.toString();
+        }
+        return batsmanName.toString();
+
     }
     private int IndividualTeamScore(Match match, String teamname) {
         int totalScore = 0 ;
