824067fa3c5193fe2403893340d590ab1f91a99e
Harish Kumar R <53997952+harishhkjg@users.noreply.github.com>
2023-11-14 15:38:56 +0530

    batsmanscore

--- src/main/java/com/cricket/serviceimpl/MatchServiceImpl.java
+++ src/main/java/com/cricket/serviceimpl/MatchServiceImpl.java
@@ -69,6 +69,7 @@ public class MatchServiceImpl implements MatchService {
     private void StartInnings(Match match, Team BattingTeam, Team BowlingTeam, int totalBalls, int Playersize) {
         int InningsScore = 0;
         int wicketCount = 0;
+        int individualruns = 0;
         List<String> EachBall = new ArrayList<>();
 
         List<String> batsmanNamedata = new ArrayList<>();
@@ -76,13 +77,16 @@ public class MatchServiceImpl implements MatchService {
             int StoreBalls = RandomBallGenerator();
             if (StoreBalls == 7) {
                 wicketCount++;
-                batsmanNamedata.add(BatsmanName(wicketCount,BattingTeam));
+                batsmanNamedata.add(BatsmanName(wicketCount,BattingTeam,individualruns));
                 EachBall.add("W");
+                individualruns=0;
             } else {
                 InningsScore += StoreBalls;
+                individualruns += StoreBalls;
                 EachBall.add(String.valueOf(StoreBalls));
             }
         }
+        batsmanNamedata.add(BatsmanName(wicketCount+1,BattingTeam,individualruns));
 
 
         match.addTeamResult(BattingTeam.getTeamname(), InningsScore, wicketCount, EachBall,batsmanNamedata);
@@ -93,115 +97,116 @@ public class MatchServiceImpl implements MatchService {
         return (int) (Math.random() * 8);
     }
 
-    private String BatsmanName(int wicketCount, Team battingTeam)
+    private String BatsmanName(int wicketCount, Team battingTeam,int individualruns)
     {
 
         String teamname = battingTeam.getTeamname();
         System.out.println(teamname);
         List<String> batsmanName = new ArrayList<>();
-
+        List<Integer> batsmanScore = new ArrayList<>();
         if(teamname.equalsIgnoreCase("india")) {
             switch (wicketCount) {
                 case 1:
                     batsmanName.add("Shubman Gill");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 2:
                     batsmanName.add("Rohit Sharma");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 3:
                     batsmanName.add("Virat Kohli");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 4:
                     batsmanName.add("shreyas Iyer");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 5:
                     batsmanName.add("KL Rahul");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 6:
                     batsmanName.add("Suryakumar Yadav");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 7:
                     batsmanName.add("Jadeja");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 8:
                     batsmanName.add("Kuldeep Yadav");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 9:
                     batsmanName.add("Mohammad Shami");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 10:
                     batsmanName.add("Jasprit Bumrah");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 11:
                     batsmanName.add("Mohammad Siraj");
-
+                    batsmanScore.add(individualruns);
                     break;
             }
 
-            return batsmanName.toString();
+            return batsmanName.toString() +"score:" + batsmanScore;
         }
         else if(teamname.equalsIgnoreCase("nz")) {
             switch (wicketCount) {
 
                 case 1:
                     batsmanName.add("Devon Conway");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 2:
                     batsmanName.add("Rachin Ravindra");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 3:
                     batsmanName.add("young");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 4:
                     batsmanName.add("Kane Wiliamson");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 5:
                     batsmanName.add("Dary Mitchell");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 6:
                     batsmanName.add("Glenn Philips");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 7:
                     batsmanName.add("Tom Latham");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 8:
                     batsmanName.add("Mitchell Santner");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 9:
                     batsmanName.add("Tim Southee");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 10:
                     batsmanName.add("Trent Boult");
-
+                    batsmanScore.add(individualruns);
                     break;
                 case 11:
                     batsmanName.add("Matt Henry");
+                    batsmanScore.add(individualruns);
 
                     break;
             }
 
-            return batsmanName.toString();
+            return batsmanName.toString() +"score:" + batsmanScore;
         }
-        return batsmanName.toString();
+        return batsmanName.toString() +"score:" + batsmanScore;
 
     }
 
